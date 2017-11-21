284dc67676eb0806f5772e513f2b0554323a8b0b
Anton Pozharskiy <apozharski@gmail.com>
2017-11-21 13:17:18 -0500

    Chase actor testing.

--- continuous/actor.py
+++ continuous/actor.py
@@ -1,4 +1,4 @@
-import random.random, random.randint
+from random import random,randint
 import math
 
 class BoundsRedefinitionException(Exception):
@@ -12,7 +12,7 @@ class ChaseActor(object):
     y_i -- Initial y position (default 0.0)
     bounds -- symetrical (x,y) bounds in tuple form (default None)
     """
-    def __init__(x_i=0.,y_i=0.,bounds = None):
+    def __init__(self,x_i=0.,y_i=0.,bounds = None):
         """
         """
         self.x = x_i
@@ -21,26 +21,26 @@ class ChaseActor(object):
         self.curr_head = (0.,0.)
         self.bounds = bounds
 
-    def walk():
+    def walk(self):
         """Function that takes a single step in the random walk."""
-        if self.curr_count > 0:
-            self.curr_head = (2*random.random()*math.pi,1+(random.random()*9))
-            self.curr_count = random.randint(1,10)
+        if self.curr_count == 0:
+            self.curr_head = (2*random()*math.pi,1+(random()*9))
+            self.curr_count = randint(1,10)
 
-        self.x += max(min(math.cos(self.curr_head[0])*self.curr_head[1],bounds[0]),-bounds[0])
-        self.y += max(min(math.sin(self.curr_head[0])*self.curr_head[1],bounds[1]),-bounds[1])
+        self.x = max(min(self.x + math.cos(self.curr_head[0])*self.curr_head[1],self.bounds[0]),-self.bounds[0])
+        self.y = max(min(self.y + math.sin(self.curr_head[0])*self.curr_head[1],self.bounds[1]),-self.bounds[1])
         self.curr_count -= 1
 
-    def set_bounds(bounds):
+    def set_bounds(self,bounds):
         """Update the bounds for the current actor. Raises BoundsRedefinitionException if redefinition is attempted."""
-        if bounds is None:
+        if self.bounds is None:
             self.bounds = bounds
         else:
             raise BoundsRedefinitionException("Bounds have already been defined for this actor. If createing a new chase use a new actor.")
     
 class StationaryActor(object):
     """This is the stationary actor in a continuous chase scenario."""
-    def __init__(x_i=0.,y_i=0.):
+    def __init__(self,x_i=0.,y_i=0.):
         """Creates a new stationary actor with initial position, default is 0,0. 
     
         Keyword arguments:
@@ -50,8 +50,17 @@ class StationaryActor(object):
         self.x = x_i
         self.y = y_i
 
-    def walk():
+    def walk(self):
         pass
     
-    def set_bounds(bounds):
+    def set_bounds(self,bounds):
         pass
+
+if __name__ == "__main__":
+    c_actor = ChaseActor()
+    c_actor.set_bounds((1000,1000))
+    for i in range(1000):
+        c_actor.walk()
+        print(c_actor.x,c_actor.y)
+        
+        
